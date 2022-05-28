862d82bd202a5030dfd7f7008b1186a884ecf942
narnarr <frc0707@gmail.com>
2022-05-29 02:38:30 +0900

    Modify: 응답코드 추가

--- src/main/java/dev/nars/wannab/util/CustomResponseStatus.java
+++ src/main/java/dev/nars/wannab/util/CustomResponseStatus.java
@@ -13,27 +13,31 @@ public enum CustomResponseStatus {
     /**
      * 400 : Client Errors (유효하지 않은 요청)
      */
-    // 이메일
+    // 회원 - 이메일
     EMPTY_EMAIL(false, 400, "이메일을 입력해주세요"),
     INVALID_EMAIL(false, 400, "이메일 형식을 확인해주세요."),
     EXISTING_EMAIL(false, 400, "이미 사용 중인 이메일입니다."),
     DELETED_EMAIL(false, 400, "탈퇴한 아이디는 재가입 및 복구할 수 없습니다."),
-    // 비밀번호
+    // 회원 - 비밀번호
     INVALID_PASSWORD(false, 400, "비밀번호는 문자, 숫자, 특수기호를 포함한 8자리 이상이어야 합니다."),
-    // jwt
+    // 회원 - jwt
     EMPTY_JWT(false, 400, "JWT를 입력해주세요."),
     INVALID_JWT(false, 400, "유효하지 않은 JWT입니다."),
     INVALID_USER_JWT(false, 400, "권한이 없는 유저의 접근입니다."),
-    // 로그인
+    // 회원 - 로그인
     USER_NOT_FOUND(false, 400, "존재하지 않는 유저입니다."),
-    INCORRECT_PASSWORD(false, 400, "잘못된 비밀번호입니다.");
+    INCORRECT_PASSWORD(false, 400, "잘못된 비밀번호입니다."),
+    // 클래스
+    COURSE_NOT_FOUND(false, 400, "존재하지 않는 클래스입니다."),
+    // 서버 오류
+    DATABASE_ERROR(false, 400, "데이터베이스 연결에 실패했습니다.");
 
 
     private final boolean isSuccess;
     private final int code;
     private final String message;
 
-    private CustomResponseStatus(boolean isSuccess, int code, String message) {
+    CustomResponseStatus(boolean isSuccess, int code, String message) {
         this.isSuccess = isSuccess;
         this.code = code;
         this.message = message;
