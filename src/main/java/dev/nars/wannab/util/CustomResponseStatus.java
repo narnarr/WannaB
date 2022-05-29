2f45d843340a22ebcfd6f583b024bc794c89d955
narnarr <frc0707@gmail.com>
2022-05-30 05:40:33 +0900

    Modify: 응답 코드 수정

--- src/main/java/dev/nars/wannab/util/CustomResponseStatus.java
+++ src/main/java/dev/nars/wannab/util/CustomResponseStatus.java
@@ -4,33 +4,35 @@ import lombok.Getter;
 
 @Getter
 public enum CustomResponseStatus {
-    //code 값은 Http Status Code 값을 따른다.
     /**
-     * 200 : Success
+     * 2000 : Success
      */
-    SUCCESS(true, 200, "요청에 성공하였습니다."),
+    SUCCESS(true, 2000, "요청에 성공하였습니다."),
 
     /**
-     * 400 : Client Errors (유효하지 않은 요청)
+     * 4000 : Client Errors (유효하지 않은 요청)
      */
     // 회원 - 이메일
-    EMPTY_EMAIL(false, 400, "이메일을 입력해주세요"),
-    INVALID_EMAIL(false, 400, "이메일 형식을 확인해주세요."),
-    EXISTING_EMAIL(false, 400, "이미 사용 중인 이메일입니다."),
-    DELETED_EMAIL(false, 400, "탈퇴한 아이디는 재가입 및 복구할 수 없습니다."),
+    EMPTY_EMAIL(false, 4001, "이메일을 입력해주세요"),
+    INVALID_EMAIL(false, 4002, "이메일 형식을 확인해주세요."),
+    EXISTING_EMAIL(false, 4003, "이미 사용 중인 이메일입니다."),
+    DELETED_EMAIL(false, 4004, "탈퇴한 아이디는 재가입 및 복구할 수 없습니다."),
     // 회원 - 비밀번호
-    INVALID_PASSWORD(false, 400, "비밀번호는 문자, 숫자, 특수기호를 포함한 8자리 이상이어야 합니다."),
+    INVALID_PASSWORD(false, 4005, "비밀번호는 문자, 숫자, 특수기호를 포함한 8자리 이상이어야 합니다."),
     // 회원 - jwt
-    EMPTY_JWT(false, 400, "JWT를 입력해주세요."),
-    INVALID_JWT(false, 400, "유효하지 않은 JWT입니다."),
-    INVALID_USER_JWT(false, 400, "권한이 없는 유저의 접근입니다."),
+    EMPTY_JWT(false, 4006, "JWT를 입력해주세요."),
+    INVALID_JWT(false, 4007, "유효하지 않은 JWT입니다."),
+    INVALID_USER_JWT(false, 4008, "권한이 없는 유저의 접근입니다."),
     // 회원 - 로그인
-    USER_NOT_FOUND(false, 400, "존재하지 않는 유저입니다."),
-    INCORRECT_PASSWORD(false, 400, "잘못된 비밀번호입니다."),
+    USER_NOT_FOUND(false, 4009, "존재하지 않는 유저입니다."),
+    INCORRECT_PASSWORD(false, 4010, "잘못된 비밀번호입니다."),
     // 클래스
-    COURSE_NOT_FOUND(false, 400, "존재하지 않는 클래스입니다."),
-    // 서버 오류
-    DATABASE_ERROR(false, 400, "데이터베이스 연결에 실패했습니다.");
+    COURSE_NOT_FOUND(false, 4011, "존재하지 않는 클래스입니다."),
+    /**
+     * 5000 : Server Errors
+     */
+    // DB 오류
+    DATABASE_ERROR(false, 5001, "데이터베이스 연결에 실패했습니다.");
 
 
     private final boolean isSuccess;
